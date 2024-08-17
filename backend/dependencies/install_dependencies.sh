e9d31d7162b7d820d5bd745c4527cc55043e3933
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-08-17 08:41:08 +0530

    Modified Dependencies

--- backend/dependencies/install_dependencies.sh
+++ backend/dependencies/install_dependencies.sh
@@ -4,29 +4,9 @@ sudo apt-get install -y python3-pip
 PYTHON_VERSION=$(python3 --version 2>&1 | awk '{print $2}' | cut -d'.' -f1,2)
 echo "Detected Python version: $PYTHON_VERSION"
 
-echo "Installing Dlib"
-
-if [[ "$PYTHON_VERSION" == "3.7" ]]; then
-    python -m pip install dlib-19.22.99-cp37-cp37m-win_amd64.whl    
-
-elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
-    python -m pip install dlib-19.22.99-cp38-cp38-win_amd64.whl
-
-elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
-    python -m pip install dlib-19.22.99-cp39-cp39-win_amd64.whl
-
-elif [[ "$PYTHON_VERSION" == "3.10" ]]; then
-    python -m pip install dlib-19.22.99-cp310-cp310-win_amd64.whl
-
-elif [[ "$PYTHON_VERSION" == "3.11" ]]; then
-    python -m pip install dlib-19.24.1-cp311-cp311-win_amd64.whl
-
-elif [[ "$PYTHON_VERSION" == "3.12" ]]; then
-    python -m pip install dlib-19.24.99-cp312-cp312-win_amd64.whl
-
-else
-    echo "Unsupported Python version. Please use Python 3.9 or 3.10."
-fi
+git clone https://github.com/davisking/dlib
+cd dlib
+python3 setup.py install
 
 echo "Installing Python Dependencies"
 
