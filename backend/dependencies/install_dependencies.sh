b150baa0b34b1149bd869b6ca42d2ea4ba445fc7
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-08-17 10:24:21 +0530

    Fixed Bugs

--- backend/dependencies/install_dependencies.sh
+++ backend/dependencies/install_dependencies.sh
@@ -4,9 +4,9 @@ sudo apt-get install -y python3-pip
 PYTHON_VERSION=$(python3 --version 2>&1 | awk '{print $2}' | cut -d'.' -f1,2)
 echo "Detected Python version: $PYTHON_VERSION"
 
-$ sudo apt-get install build-essential cmake
-$ sudo apt-get install libgtk-3-dev
-$ sudo apt-get install libboost-all-dev
+sudo apt-get install build-essential cmake
+sudo apt-get install libgtk-3-dev
+sudo apt-get install libboost-all-dev
 
 echo "Installing Python Dependencies"
 
